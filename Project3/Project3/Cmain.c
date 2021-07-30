1379790342e0c4c5fdb91492c0ba7f056f6d6652
재욱 이 <qqxpp2468@gmail.com>
2021-07-30 11:12:15 +0900

    test1

--- Project3/Project3/Cmain.c
+++ Project3/Project3/Cmain.c
@@ -33,30 +33,83 @@ int main()
 	//	min = c;
 	//}
 
-	int a;
-	do
-	{	
+	//int a;
+	//do
+	//{	
+	//
+	//	scanf(" %d", &a);
+	//
+	//
+	//	switch (a)
+	//	{
+	//	case 1:
+	//		printf("1");
+	//		break;
+	//	case 2:
+	//		printf("2");
+	//	case 3:
+	//		printf("3");
+	//		break;
+	//	default:
+	// 	   
+	//		break;
+	//	}
+	//} while (a != 9);
+	//int a = 0; 
+	//
+	//while (3)
+	//{
+	//	printf("메롱메롱메롱이다롱\n");
+	//	scanf(" %d", &a);
+	//	printf(" %d", a);
+	//}
+	//int a =0; 
+	//
+	//while (a <10)
+	//{
+	//	printf("메롱\n");
+	//	a++;
+	//}
+	//
+	//int b = 0;
+	//printf("몇 번 반복해? : \n");
+	//scanf(" %d", &a);
+	//while (b < a)
+	//{
+	//	printf("메로나\n");
+	//	
+	//		b++;
+	//}
+	//-------------------------------------------
+	//int a ;
+	//int b = 0;
+	//int c = 0;
+	//printf("몇 줄 만들어? : \n");
+	//scanf(" %d", &a);
+	//while (b < a)//b가 a가 될때까지 *을 반복한다. 
+	//{
+	//	c = 0;//별그릴 위치 초기화 
+	//	while (c < a) {
+	//		c++;//별 반복하기 
+	//		printf("* ");//별그리기
+	//	}
+	//	printf("\n");//줄바꾸기
+	//	b++;//줄내리기
+	//}
 
-		scanf(" %d", &a);
-
-
-		switch (a)
-		{
-		case 1:
-			printf("1");
-			break;
-		case 2:
-			printf("2");
-		case 3:
-			printf("3");
-			break;
-		default:
-			break;
-		}
-	} while (a != 9);
-
-
-
-
-
+	//----------------------------------------
+	int i=0;
+	while (i++ < 10)// i는 0부터 시작
+	{
+		printf("%d", i);
+	}
+	printf("\n%d", i);
+	printf("\n\n");
+	i = 0;
+	while (++i < 10)// i는 1부터 시작
+	{
+		printf("%d", i);
+	}
+	printf("\n%d", i);
+	//-------------------------------------
 }
