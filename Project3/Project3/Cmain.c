37e43f012980506a81690b46f4bfade27ddcdca6
재욱 이 <qqxpp2468@gmail.com>
2021-07-30 09:46:30 +0900

    test2

--- Project3/Project3/Cmain.c
+++ Project3/Project3/Cmain.c
@@ -3,6 +3,35 @@
 int main()
 
 {
+	//int a = 10; 
+	//int b = 20;
+	//int c;
+	//
+	//int max;
+	//int min;
+	//
+	//if (a > b)
+	//{
+	//	max = a;
+	//	min = b;
+	//}
+	//else
+	//{
+	//	max = b;
+	//	min = a;
+	//}
+	//printf("정수를 입력하세욥 : ");
+	//scanf(" %d", &c);
+	//
+	//if (max < c)
+	//{
+	//	max = c;
+	//}
+	//else if (min > c)
+	//{
+	//	min = c;
+	//}
 
-	printf("sdsd");
+
+
 }
